c5298ae436978dc833e7991dc66d043506de627d
David Burris <37857537+dburris554@users.noreply.github.com>
2018-04-20 20:23:48 -0500

    added notes for changes

--- AvlTree.h
+++ AvlTree.h
@@ -18,19 +18,18 @@ public:
 	virtual ~AvlTree();
 	void addItem(Eclipse* myEclipse);
 	TreeNode* getRootNode();
-	TreeNode* getFoundNode();
-	void setFoundNode(TreeNode* myFoundNode);
-	void removeItem(Eclipse* myEclipse); //you can remove a node with an eclipse or key
+	TreeNode* getFoundNode(); //refactor to TempNode
+	void setFoundNode(TreeNode* myFoundNode); //refactor to tempNode
+	void removeItem(Eclipse* myEclipse); //use tempNode to call updateTree after removing item
 	Eclipse* findEclipse(int key);
 	TreeNode* findNode(int key);
-	TreeNode* findNodeForRemoval(int key);
 	void printPreOrder();
 	void printInOrder();
 	void printPostOrder();
 	void copyToArray(ResizeableArray<Eclipse>& myEclipses);
 
 private:
-	bool balanceTree(TreeNode* insertedNode); //use balanceResult to determine rotation
+	bool balanceTree(TreeNode* insertedNode); //starts at inserted/deleted location, updates balanceFactors and checks for rotations back to rootNode
 	void leftHeavyRotate(TreeNode* nodeA);
 	void leftInnerRotate(TreeNode* nodeA);
 	void rightHeavyRotate(TreeNode* nodeA);
@@ -40,11 +39,11 @@ private:
 	void printInOrder(TreeNode* curNode); //recurse print in-order
 	void printPostOrder(TreeNode* curNode); //recurse print post-order
 	void copyToArray(ResizeableArray<Eclipse>& myEclipses, TreeNode* firstNode);
-	void find(TreeNode* curNode, int key, bool &forRemoval);
+	void find(TreeNode* curNode, int key, bool &forRemoval); //make sure this is updated
 	int computeBalanceFactor(TreeNode* myNode);
-	void computeBalanceFactor(TreeNode* myNode, int &leftDepth, int &rightDepth);
+	void computeBalanceFactor(TreeNode* myNode, int &leftDepth, int &rightDepth); //resolve infinite loop here
 	TreeNode* rootNode;
-	TreeNode* foundNode;
+	TreeNode* foundNode; //refactor this to tempNode
 };
 
 #endif /* AVLTREE_H_ */
